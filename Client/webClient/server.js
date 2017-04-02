c0baaa2a407243d1961522b5b0e3e6b070fd6d90
Alexandre GC <a.gaspardcilia@gmail.com>
2017-04-03 01:37:14 +0200

    Le client peut recevoir et envoyer avec la server. Le chat est connecté.

--- Client/webClient/server.js
+++ Client/webClient/server.js
@@ -4,10 +4,6 @@ var io = require('socket.io')(http);
 var util = require('util');
 var net = require('net');
 
-var port;
-var serverAdress;
-var client = new net.Socket();
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -20,24 +16,57 @@ http.listen(3000, function(){
 
 
 io.on('connection', function(socket){
+    var port;
+    var serverAdress;
+    var username;
+    var server = new net.Socket();
+    var isServerConnected = false;
 
-    socket.on('serverConnectionRequest', function(r_address, r_port) {
-        if (r_address != undefined && r_port != undefined) {
+    socket.on('send', function(data) {
+        console.log("writing : " + data);
+        server.write(data);
+    });
+
+    socket.on('serverConnectionRequest', function(r_address, r_port, r_username) {
+        if (r_address != undefined && r_port != undefined && r_username != undefined) {
+            if (r_port < 0 || r_port > 65536) {
+                socket.emit('connectionRequestAnswer', "Numéro de port incorrect.", 1);
+                return;
+            }
+
+
             port = r_port;
             serverAdress = r_address;
+            username = r_username;
 
-            initSocket();
-            io.emit('connectionRequestAnswer', "ok", 0);
-        }
-        else {
-            io.emit('connectionRequestAnswer', "ko", 1);
+
+
+            server = net.createConnection(port, serverAdress).on("connect", function(e) {
+                console.log("Server connection succesful");
+
+                socket.emit('connectionRequestAnswer', "ok", 0);
+            }).on("error", function(e) {
+                console.log("Can't connect to server.");
+                socket.emit('connectionRequestAnswer', "Impossible de joindre le serveur de Scrabble.", 1);
+            });
+
+            server.on('data', function(data) {
+                console.log("Received : " + data);
+                socket.emit("receive", data.toString('utf8'));
+            });
+
+
+
+        } else {
+            socket.emit('connectionRequestAnswer', 'Too few args.', 1);
         }
     });
+
+    socket.on('disconnect', function() {
+        console.log('Disconnection.');
+        server.destroy();
+    });
 });
-
-function initSocket() {
-
-}
 
 
 
